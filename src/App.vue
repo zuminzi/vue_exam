7a1a0dfda222a7c6bbe9c5a37326df5d429846d8
Ju Minji <alswl0w0@gmail.com>
2023-01-03 00:53:52 +0900

    CSS, array/object

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <!-- template: HTML, CSS 등의 마크업 속성과 뷰 인스턴스에서 정의한 데이터 및 로직들을 연결하여 사용자가 브라우저에서 볼 수 있는 형태의 HTML로 변환해주는 속성 -->
 <template>
+  <div class="menu">
+    <a>Home</a>
+    <a>Products</a>
+    <a>About</a>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <div>
     Vuedongsan <!--실시간으로 변경될 일이 없는 데이터는 굳이 데이터바인딩 X -->
-    <h4> xx 원룸 </h4>
+    <h4> {{ products[0] }} </h4>
     <p> {{ price1 }} 만원</p> <!--{{데이터바인딩 문법}} -->
   </div>
   <div>
-    <h4> xx 원룸 </h4>
+    <h4> {{ products[1] }} </h4>
     <p> {{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4> {{ products[2] }} </h4>
+    <p> 50 만원</p>
   </div>
 </template>
 
@@ -23,6 +33,7 @@ export default {
       // 데이터바인딩 장점? 실시간 자동 렌더링
       price1 : 60,
       price2 : 70,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
   components: {
@@ -39,4 +50,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
